5fe0da9ec66bd8d1b90a814d9a9e83900d3eb81a
Vishnu  Janardhan Bathula <152680952+vishnujanardhan003@users.noreply.github.com>
2025-01-24 10:09:15 +0530

    dom.js

--- dom.js
+++ dom.js
@@ -6,14 +6,27 @@
 // div.innerHTML="<h1>Header</h1>"
 // console.log(div);
 
-let ele=document.getElementsByClassName("test");
-// console.log(ele);
-// console.log(Array.isArray(ele));
-ele[0].style.background="yellow";
+// let ele=document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].style.background="yellow";
 
-let x=[...ele];
-console.log(x,Array.isArray(x));
-x.map((element)=>{
-    // console.log(element);
-    // element.style.backgroundColor="teal";
-})
+// let x=[...ele];
+// console.log(x,Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(element);
+//     // element.style.backgroundColor="teal";
+// })
+
+// let ele=document.querySelector("demo");
+// console.log(ele);
+
+// let ele=document.querySelector(".test");
+// console.log(ele);
+
+
+// let ele=document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map((element)=>{
+//     console.log(element.innerText);
+// })
